b58db809950d9a32a20aebb98f0f60d50ff36ef5
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-05-25 15:24:30 -0700

    roll button event

--- script.js
+++ script.js
@@ -5,25 +5,19 @@ const playerTwo = document.querySelector(".score2");
 const newGame = document.querySelector(".new-game");
 const roll = document.querySelector(".roll");
 const endGame = document.querySelector(".end-game");
-//Randomizing a number from 1 to 6
-let number =  Math.floor((Math.random() * 6) + 1);
 
 //dice 1
 let imgOne = document.createElement("img");
-imgOne.src = `./images/dice-${number}.png`;
+dice.appendChild(imgOne);
 
 // dice 2
 let imgTwo = document.createElement("img");
-imgTwo.src = `./images/dice-${number}.png`;
-
+diceTwo.appendChild(imgTwo);
 
 //Roll effect
 roll.addEventListener("click", () => {
-    dice.appendChild(imgOne);
-    diceTwo.appendChild(imgTwo);
-  
-})
-
-
-
-
+  //Randomizing a number from 1 to 6
+  let number = Math.floor(Math.random() * 6 + 1);
+  imgOne.src = `./images/dice-${number}.png`;
+  imgTwo.src = `./images/dice-${number}.png`;
+});
