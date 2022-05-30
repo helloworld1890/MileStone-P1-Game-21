e3a6ef9c8bce0ad2316c0a89c0dbe88450d3b137
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-05-29 20:31:45 -0700

    displaying the players score

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-const dice = document.querySelector(".dice");
-const diceTwo = document.querySelector(".dice2");
+const dice = document.querySelector("#diceImg");
+const diceTwo = document.querySelector("#diceImg2");
 const playerOne = document.querySelector(".score1");
 const playerTwo = document.querySelector(".score2");
 const newGame = document.querySelector(".new-game");
 const roll = document.querySelector(".roll");
 const endGame = document.querySelector(".end-game");
+const hold = document.querySelector(".hold");
 
-//dice 1
-let imgOne = document.createElement("img");
-dice.appendChild(imgOne);
-
-// dice 2
-let imgTwo = document.createElement("img");
-diceTwo.appendChild(imgTwo);
+let score, playerScore, activePlayer, gamePlay, attempts;
+score = [0, 0];
+playerScore = 0;
+activePlayer = 0;
+gamePlay = true;
+attempts = 3;
 
 //Roll effect
 roll.addEventListener("click", () => {
   //Randomizing a number from 1 to 6
-  let number = Math.floor(Math.random() * 6 + 1);
-  imgOne.src = `./images/dice-${number}.png`;
-  imgTwo.src = `./images/dice-${number}.png`;
+  let rollDice1 = Math.floor(Math.random() * 6 + 1);
+  let rollDice2 = Math.floor(Math.random() * 6 + 1);
+
+  dice.src = `./images/dice-${rollDice1}.png`;
+  diceTwo.src = `./images/dice-${rollDice2}.png`;
+
+  //Player logic Adding points
+  if (rollDice1 !== 0 && rollDice2 !== 0) {
+    playerScore = playerScore + rollDice1 + rollDice2;
+    playerOne.textContent = playerScore;
+  } else {
+      nextPlayer();
+  }
 });
+
+
+
+const nextPlayer = () => {
+    activePlayer === 0 ? activePlayer === 1 : activePlayer === 0;
+}
