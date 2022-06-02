fe8f84b38dca8ed3731e1083566e6d323e283036
Craig Caparaz <cappuccino1890@Craigs-Air.lan1>
2022-06-01 18:43:47 -0700

    fixing the players turn

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const dice = document.querySelector("#diceImg");
 const diceTwo = document.querySelector("#diceImg2");
-const playerOne = document.querySelector(".score1");
-const playerTwo = document.querySelector(".score2");
 const newGame = document.querySelector(".new-game");
 const roll = document.querySelector(".roll");
 const endGame = document.querySelector(".end-game");
 const hold = document.querySelector(".hold");
 
+// const playerTwo = document.querySelector(`.score2`);
 let score, playerScore, activePlayer, gamePlay, attempts;
 score = [0, 0];
 playerScore = 0;
-activePlayer = 0;
+turnPlayer = 0;
 gamePlay = true;
-attempts = 3;
+attempts = 0;
 
 //Roll effect
 roll.addEventListener("click", () => {
@@ -24,16 +23,24 @@ roll.addEventListener("click", () => {
   diceTwo.src = `./images/dice-${rollDice2}.png`;
 
   //Player logic Adding points
-  if (rollDice1 !== 0 && rollDice2 !== 0) {
+  if (attempts === 0 || attempts === 1 || attempts === 2) {
+    attempts += 1;
+    console.log(attempts);
     playerScore = playerScore + rollDice1 + rollDice2;
-    playerOne.textContent = playerScore;
-  } else {
-      nextPlayer();
+    let player = document.querySelector(`.score${turnPlayer}`);
+    player.textContent = playerScore;
+  } else if (attempts === 3) {
+    nextPlayer();
+    attempts = 0;
   }
 });
 
-
-
 const nextPlayer = () => {
-    activePlayer === 0 ? activePlayer === 1 : activePlayer === 0;
-}
+  turnPlayer === 0 ? (turnPlayer = 1) : (turnPlayer = 0);
+  alert("Cheaters never prosper, you only get three rolls...");
+  playerScore = 0;
+  document.querySelector(".score0").textContent = "0";
+  document.querySelector(".score1").textContent = "0";
+  dice.src = `./images/dice-1.png`;
+  diceTwo.src = `./images/dice-2.png`;
+};
