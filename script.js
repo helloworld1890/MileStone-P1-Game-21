d5264cc0064b882b22407e2785a6ec13cb026548
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-06-05 16:15:17 -0700

    visual active player

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const newGame = document.querySelector(".new-game");
 const roll = document.querySelector(".roll");
 const endTurn = document.querySelector(".end-turn");
 const holdButton = document.querySelector(".hold");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+//initial active player
+player1.style.backgroundColor = "#E3E1E1";
+
 
 // const playerTwo = document.querySelector(`.score2`);
 let score, playerScore, gamePlay, attempts, turnPlayer;
@@ -15,7 +20,7 @@ attempts = 0;
 
 //reset the results
 const reset = () => {
-    score = [0, 0]
+    score = [0, 0];
     playerScore = 0;
     attempts = 0;
     document.querySelector(".score0").textContent = "0";
@@ -50,7 +55,15 @@ roll.addEventListener("click", () => {
 
 //function that calls on the player to play, beware to not over roll the dices
 const nextPlayer = () => {
-  turnPlayer === 0 ? (turnPlayer = 1) : (turnPlayer = 0);
+if (turnPlayer === 0)  {
+    turnPlayer = 1
+    player2.style.backgroundColor = "#E3E1E1";
+    player1.style.backgroundColor = "#f8f8f8";
+} else {
+    turnPlayer = 0;
+    player1.style.backgroundColor = "#E3E1E1";
+    player2.style.backgroundColor = "#f8f8f8";
+} 
   reset();
 };
 
@@ -64,20 +77,22 @@ endTurn.addEventListener("click", () => {
 
 //Start new game 
 newGame.addEventListener("click", () => {
+    turnPlayer === 0 ? (turnPlayer = 1) : (turnPlayer = 0);
     if (turnPlayer === 1) {
         turnPlayer = 0
     }
     reset()
 });
 
+
 //Building the Hold button 
 holdButton.addEventListener("click", () => {
     if (gamePlay) {
         score[turnPlayer] += playerScore;
         document.querySelector(`.scoreBoard-${turnPlayer}`).textContent = score[turnPlayer];
-
         //winning condition
         if (score[turnPlayer] === 21) {
+            document.getElementById(`player${turnPlayer}`).style.backgroundColor = "#8ff879";
             alert("You have won the game of 21!!! congrats");
             reset();
             gamePlay = false;
@@ -87,6 +102,12 @@ holdButton.addEventListener("click", () => {
         } 
     }
 });
+
+//how to switch between active and none active 
+
+
+
+
 
 
 
