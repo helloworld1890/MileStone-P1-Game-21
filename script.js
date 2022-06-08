86501bf9ec08fa8b7c2b5a2e6d521eb247ecdb23
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-06-08 00:28:25 -0700

    fixing the score board

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const player2 = document.getElementById("player2");
 //initial active player
 player1.style.backgroundColor = "#E3E1E1";
 
-
 // const playerTwo = document.querySelector(`.score2`);
 let score, playerScore, gamePlay, attempts, turnPlayer;
 score = [0, 0];
@@ -20,16 +19,18 @@ attempts = 0;
 
 //reset the results
 const reset = () => {
-    score = [0, 0];
-    playerScore = 0;
-    attempts = 0;
-    document.querySelector(".score0").textContent = "0";
-    document.querySelector(".score1").textContent = "0";
-    dice.src = `./images/dice-1.png`;
-    diceTwo.src = `./images/dice-2.png`;
-    document.querySelector(`.scoreBoard-0`).textContent = "0";
-    document.querySelector(`.scoreBoard-1`).textContent = "0";
-}
+  score = [0, 0];
+  playerScore = 0;
+  attempts = 0;
+  document.querySelector(".score0").textContent = "0";
+  document.querySelector(".score1").textContent = "0";
+  dice.src = `./images/dice-1.png`;
+  diceTwo.src = `./images/dice-2.png`;
+  document.querySelector(`.scoreBoard-0`).textContent = "0";
+  document.querySelector(`.scoreBoard-1`).textContent = "0";
+  player1.style.backgroundColor = "#E3E1E1";
+  player2.style.backgroundColor = "#f8f8f8";
+};
 
 //Roll effect
 roll.addEventListener("click", () => {
@@ -55,59 +56,65 @@ roll.addEventListener("click", () => {
 
 //function that calls on the player to play, beware to not over roll the dices
 const nextPlayer = () => {
-if (turnPlayer === 0)  {
-    turnPlayer = 1
+  if (turnPlayer === 0) {
+    turnPlayer = 1;
     player2.style.backgroundColor = "#E3E1E1";
     player1.style.backgroundColor = "#f8f8f8";
-} else {
+  } else {
     turnPlayer = 0;
     player1.style.backgroundColor = "#E3E1E1";
     player2.style.backgroundColor = "#f8f8f8";
-} 
-  reset();
+  }
+  playerScore = 0;
+  attempts = 0;
+  document.querySelector(".score0").textContent = "0";
+  document.querySelector(".score1").textContent = "0";
+  dice.src = `./images/dice-1.png`;
+  diceTwo.src = `./images/dice-2.png`;
 };
-
-
 
 //End your turn if you over shot the dices
 endTurn.addEventListener("click", () => {
+  nextPlayer();
+});
+
+//Start new game
+newGame.addEventListener("click", () => {
   turnPlayer === 0 ? (turnPlayer = 1) : (turnPlayer = 0);
+  if (turnPlayer === 1) {
+    turnPlayer = 0;
+  }
   reset();
 });
 
-//Start new game 
-newGame.addEventListener("click", () => {
-    turnPlayer === 0 ? (turnPlayer = 1) : (turnPlayer = 0);
-    if (turnPlayer === 1) {
-        turnPlayer = 0
-    }
-    reset()
-});
-
-
-//Building the Hold button 
+//Building the Hold button
 holdButton.addEventListener("click", () => {
-    if (gamePlay) {
-        score[turnPlayer] += playerScore;
-        document.querySelector(`.scoreBoard-${turnPlayer}`).textContent = score[turnPlayer];
-        //winning condition
-        if (score[turnPlayer] === 21) {
-            document.getElementById(`player${turnPlayer}`).style.backgroundColor = "#8ff879";
-            alert("You have won the game of 21!!! congrats");
-            reset();
-            gamePlay = false;
-        } else if (score[turnPlayer] > 21) {
-            score = [0, 0];
-            nextPlayer();
-        } 
+  if (gamePlay) {
+    score[turnPlayer] += playerScore;
+    document.querySelector(`.scoreBoard-${turnPlayer}`).textContent =
+      score[turnPlayer];
+    //winning condition
+    if (score[turnPlayer] === 21) {
+      alert("You have won the game of 21!!! congrats");
+      gamePlay = false;
+      turnPlayer = 0;
+      reset();
+    } else if (score[turnPlayer] >= 22) {
+      if (turnPlayer === 0) {
+        turnPlayer = 0;
+        score[turnPlayer] = 0;
+        document.querySelector(`.scoreBoard-0`).textContent = score[turnPlayer];
+        nextPlayer();
+      } else if (turnPlayer === 1) {
+        turnPlayer = 1;
+        score[turnPlayer] = 0;
+        document.querySelector(`.scoreBoard-1`).textContent = score[turnPlayer];
+        nextPlayer();
+      }
+    } else {
+      nextPlayer();
     }
+  }
 });
 
-//how to switch between active and none active 
-
-
-
-
-
-
-
+//how to switch between active and none active
